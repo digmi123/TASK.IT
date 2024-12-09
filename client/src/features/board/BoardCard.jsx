2cfcdfeb87ce779777d7d1b901f7e1b3bea2aa47
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-12-09 13:47:22 +0200

    Show delete button only if current user is owner.

--- client/src/features/board/BoardCard.jsx
+++ client/src/features/board/BoardCard.jsx
@@ -1,8 +1,9 @@
 import { Button } from "@/components/ui/button";
 import Card from "./components/Card";
 import { Link } from "react-router-dom";
+import { useUser } from "@/shared/providers/UserProvider";
 export default function BoardCard({ boardInfo, id }) {
-  console.log({ boardInfo });
+  const { userData } = useUser();
 
   return (
     <Card>
@@ -11,7 +12,13 @@ export default function BoardCard({ boardInfo, id }) {
         <div id="img-section" className="bg-slate-900 h-[100px]" />
         <div id="board-content" className="p-4 flex flex-col gap-3">
           <h3 className="">{boardInfo.name}</h3>
-          <Button variant="secondary">Edit Board</Button>
+          <div
+            id="board-action-buttons"
+            className="flex items-center gap-4 justify-between"
+          >
+            <Button variant="secondary">Edit Board</Button>
+            {userData.id === boardInfo.owner_id && <Button>Delete</Button>}
+          </div>
         </div>
       </Link>
     </Card>
