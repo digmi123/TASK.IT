c539802b95e8d328300ae68d32e98043b5c5eb33
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-12-17 15:18:58 +0200

    Update Task card on column.

--- client/src/features/tasks/components/Task.jsx
+++ client/src/features/tasks/components/Task.jsx
@@ -1,4 +1,10 @@
 import { useDraggable } from "@dnd-kit/core";
+import Avatar from "@/assets/user.svg?react";
+import CheckMark from "@/assets/checkmark.svg?react";
+import CommentsIcon from "@/assets/comments.svg?react";
+import taskBg from "@/assets/task-bg.jpg";
+import Tag from "./Tag";
+import TaskDialog from "./TaskDialog";
 
 function Task({ task }) {
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
@@ -13,16 +19,32 @@ function Task({ task }) {
     : undefined;
 
   return (
-    <div
-      className="bg-slate-600 p-4 rounded-md w-48"
-      ref={setNodeRef}
-      style={style}
-      {...listeners}
-      {...attributes}
-    >
-      <h1 className="font-bold text-lg">{task.title}</h1>
-      <p>{task.description}</p>
-    </div>
+    <TaskDialog task={task}>
+      <div
+        className="bg-white p-4 rounded-md max-w-60 shadow flex flex-col items-start gap-4"
+        ref={setNodeRef}
+        style={style}
+        {...listeners}
+        {...attributes}
+      >
+        <img src={taskBg} alt="bg" className="rounded-md" />
+        <div className="flex gap-2">
+          <CheckMark style={{ width: "24px", height: "24px" }} />
+          <h1 className="">{task.title}</h1>
+        </div>
+        <Tag priority={task.priority} />
+
+        <div className="flex items-center justify-between gap-2 w-full">
+          <div className="flex items-center gap-2">
+            <Avatar />
+            {/* <p>{task.createdAt}</p> */}
+            <p className="text-foreground">Apr 11-18</p>
+          </div>
+          <CommentsIcon />
+        </div>
+        {/* Add Likes and comments icons on the bottom-right side of the card */}
+      </div>
+    </TaskDialog>
   );
 }
 
