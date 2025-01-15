88c8cadef63ce4999d689d8f98df39d94b131ce4
digmi123 <65775879+digmi123@users.noreply.github.com>
2025-01-15 11:55:56 +0200

    Remove unused functions on boardProvider.

--- client/src/shared/providers/BoardProvider.jsx
+++ client/src/shared/providers/BoardProvider.jsx
@@ -1,39 +1,12 @@
-import { useGetBoard } from "@/features/board/hooks/useGetBoard";
-import { addColumn } from "@/features/columns/api";
 import { createContext, useContext } from "react";
 import { Outlet } from "react-router-dom";
 
 const BoardContext = createContext();
 
 export default function BoardProvider({ boardId }) {
-  const { boardData, setBoardData, loading } = useGetBoard({ boardId });
-
-  if (loading) return <div>Loading...</div>;
-
-  const handleAddColumn = (columName) => {
-    // Optimistic update
-    const newColumn = {
-      id: Math.random(),
-      name: columName,
-      tasks: [],
-    };
-
-    setBoardData((board) => {
-      const updatedBoardData = {
-        ...board,
-        columns: [...board.columns, newColumn],
-      };
-
-      return updatedBoardData;
-    });
-
-    // API call
-    addColumn({ boardId, columnName: columName });
-  };
-
   return (
     <BoardContext.Provider
-      value={{ boardData, setBoardData, loading, handleAddColumn }}
+    // value={{ boardData, setBoardData, loading, handleAddColumn }}
     >
       <Outlet />
     </BoardContext.Provider>
