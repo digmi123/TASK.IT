8f172b263b8a3c214004c72c851ea45ccbde4e0f
digmi123 <65775879+digmi123@users.noreply.github.com>
2025-01-15 11:41:37 +0200

    Extracting component to different file and handle logic there.

--- client/src/features/columns/components/Columns.jsx
+++ client/src/features/columns/components/Columns.jsx
@@ -1,22 +1,14 @@
-import { Button } from "@/components/ui/button";
 import Column from "./Column";
-import { useState } from "react";
-import NewColumnForm from "./NewColumnForm";
+import NewColumn from "./NewColumn";
 
 function Columns({ columns }) {
-  const [askColumnName, setAskColumnName] = useState(false);
-
   return (
     <div className="flex gap-3 p-4 flex-1 overflow-x-auto">
       {columns.map((column) => (
         <Column key={column.id} column={column} />
       ))}
 
-      {askColumnName ? (
-        <NewColumnForm setOpen={setAskColumnName} />
-      ) : (
-        <Button onClick={() => setAskColumnName(true)}>Add Column</Button>
-      )}
+      <NewColumn />
     </div>
   );
 }
