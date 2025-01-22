3fad83cd915d4153f37aa6be693aad63ca81dd1f
digmi123 <65775879+digmi123@users.noreply.github.com>
2025-01-22 18:44:38 +0200

    Update AppLayout and remove providers.

--- client/src/shared/layouts/AppLayout.jsx
+++ client/src/shared/layouts/AppLayout.jsx
@@ -1,20 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import UserProvider from "../providers/UserProvider";
-import DesksProvider from "../providers/DesksProvider";
 import { Provider } from "react-redux";
 import { store } from "@/redux/store";
 
 function AppLayout() {
   return (
-    <UserProvider>
-      <Provider store={store}>
-        <DesksProvider>
-          <Navbar />
-          <Outlet />
-        </DesksProvider>
-      </Provider>
-    </UserProvider>
+    <Provider store={store}>
+      <Navbar />
+      <Outlet />
+    </Provider>
   );
 }
 
