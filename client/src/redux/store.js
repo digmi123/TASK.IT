cbc77fa7b0186329788dad36182bf5b15a7e8c84
digmi123 <65775879+digmi123@users.noreply.github.com>
2025-01-22 18:30:42 +0200

    Add user redux provider to redux store.

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import desksReducer from "./slices/desksSlice";
 import boardReducer from "./slices/boardSlice";
+import userReducer from "./slices/userSlice";
 
 export const store = configureStore({
   reducer: {
     desks: desksReducer,
     board: boardReducer,
+    user: userReducer,
   },
 });
