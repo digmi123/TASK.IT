09af5702a79c0a595a382c712b21a8803c94cca1
digmi123 <65775879+digmi123@users.noreply.github.com>
2025-01-22 18:43:31 +0200

    Hnadle member picture on member component.

--- client/src/features/members/components/Member.jsx
+++ client/src/features/members/components/Member.jsx
@@ -5,9 +5,13 @@ export default function Member({ member }) {
   return (
     <div className="flex items-center justify-between gap-4 border-b-2 py-2">
       <div id="member-area" className="flex items-center gap-4">
-        <img src={reactLogo} alt="" className="w-12 h-12" />
+        {member.picture ? (
+          <img src={member.picture} alt="" className="w-12 h-12 rounded-full" />
+        ) : (
+          <img src={reactLogo} alt="" className="w-12 h-12 rounded-full" />
+        )}
+
         <div id="member-info" className="flex flex-col gap-2">
-          <h2 className="font-semibold text-xl">Member Name</h2>
           <p>{member.email}</p>
         </div>
       </div>
