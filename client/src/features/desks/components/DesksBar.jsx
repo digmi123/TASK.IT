909830b7d9caa6ba07fcd017d507352294b46f33
digmi123 <65775879+digmi123@users.noreply.github.com>
2025-02-02 13:43:20 +0200

    Desks list on sidebar.

---
+++ client/src/features/desks/components/DesksBar.jsx
@@ -0,0 +1,30 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import Desk from "@/assets/desk.svg?react";
+
+function DesksBar() {
+  const { desks } = useSelector((state) => state.desks);
+
+  return (
+    <div id="desks">
+      <h2 className="font-semibold text-xl my-2">My desks</h2>
+      <div className="flex flex-col gap-4">
+        {desks.map((desk) => (
+          <Link key={desk.id} to={`/${desk.id}`}>
+            <li className="flex items-center gap-4">
+              <div
+                id="desk-icon"
+                className="bg-primary rounded-md p-1 flex items-center justify-center"
+              >
+                <Desk style={{ color: "white" }} />
+              </div>
+              {desk.name}
+            </li>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default DesksBar;
