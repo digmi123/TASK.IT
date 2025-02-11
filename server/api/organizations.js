da944b1c0a6b033b21f2fa75eea16d6c49b1d9bb
digmi123 <65775879+digmi123@users.noreply.github.com>
2025-02-11 13:03:10 +0200

    Implement organization endpoints to retrieve single and all data.

---
+++ server/api/organizations.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
+const { Op } = require("sequelize");
+
+router.get("/", async (req, res) => {
+  const loggedUser = req.user;
+  const organizations = await db.Organizations.findAll({
+    where: {
+      [Op.or]: [{ "$members.id$": loggedUser.id }, { owner_id: loggedUser.id }],
+    },
+    include: [
+      {
+        model: db.User,
+        as: "owner",
+      },
+      {
+        model: db.User,
+        as: "members",
+      },
+    ],
+  });
+
+  organizations.forEach((organization) =>
+    organization.members.push(organization.owner)
+  );
+  return res.json(organizations);
+});
+
+router.get("/:organizationId", async (req, res) => {
+  const { organizationId } = req.params;
+  const organization = await db.Organizations.findOne({
+    where: { id: organizationId },
+    include: [
+      {
+        model: db.User,
+        as: "owner",
+      },
+      {
+        model: db.User,
+        as: "members",
+      },
+    ],
+  });
+  return res.json(organization);
+});
+
+module.exports = router;
