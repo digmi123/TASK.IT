70b8ebc92d40441848e94999474296adaa2add11
digmi123 <65775879+digmi123@users.noreply.github.com>
2025-02-11 12:49:50 +0200

    Remove unused imports.

--- client/src/features/task/components/NewTaskDialog.jsx
+++ client/src/features/task/components/NewTaskDialog.jsx
@@ -8,8 +8,6 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-// import { Input } from "@/components/ui/input";
-// import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import NewTask from "../forms/NewTask";
