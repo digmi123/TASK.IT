6de657a2533e1e49cb2fb9e4be9b73ccc6ee53dd
digmi123 <65775879+digmi123@users.noreply.github.com>
2025-02-11 12:57:09 +0200

    Remove wrapper div and its styling.

--- client/src/features/screens/Home.jsx
+++ client/src/features/screens/Home.jsx
@@ -1,19 +1,15 @@
-import Desks from "@/features/screens/Desks";
+import Desks from "@/features/desks/components/Desks";
 import Templates from "@/features/desks/components/Templates";
 import StarredBoards from "@/features/desks/components/StarredBoards";
-import Sidebar from "@/features/desks/components/Sidebar";
 
-function HomeLayout() {
+function Home() {
   return (
-    <div id="content" className="flex justify-center gap-16 py-8 px-12">
-      <Sidebar />
-      <main className="flex flex-col gap-6">
-        <Templates />
-        <StarredBoards />
-        <Desks />
-      </main>
-    </div>
+    <>
+      <Templates />
+      <StarredBoards />
+      <Desks />
+    </>
   );
 }
 
-export default HomeLayout;
+export default Home;
