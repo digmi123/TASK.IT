078e0b732e587959fb7a0bc8d8f424bdcd72c085
digmi123 <65775879+digmi123@users.noreply.github.com>
2025-02-11 12:58:28 +0200

    Extract Desks components to New component.

--- client/src/features/screens/Desks.jsx
+++ client/src/features/screens/Desks.jsx
@@ -1,31 +1,7 @@
-import TemplateIcon from "@/assets/template.svg?react";
-import DeskSection from "../desks/components/DeskSection";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchDesksThunk } from "@/redux/slices/desksSlice";
-import { useEffect } from "react";
+import DesksList from "@/features/desks/components/Desks";
 
-export default function Desks() {
-  const dispatch = useDispatch();
-  const { desks, loading } = useSelector((state) => state.desks);
-  // Fetch desks on mount
-  useEffect(() => {
-    dispatch(fetchDesksThunk());
-  }, [dispatch]);
-
-  if (loading) return <div>Loading...</div>;
-
-  return (
-    <div id="section-wrapper" className="flex flex-col gap-2">
-      <div className="flex items-center gap-4">
-        <TemplateIcon />
-        <h2 className="text-2xl font-semibold">Your Desks</h2>
-      </div>
-
-      <div id="desk-section-wrapper" className="flex flex-col gap-6">
-        {desks.map((desk) => (
-          <DeskSection key={desk.id} desk={desk} />
-        ))}
-      </div>
-    </div>
-  );
+function Desks() {
+  return <DesksList />;
 }
+
+export default Desks;
