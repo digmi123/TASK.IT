88a3638956f5276e1816aee8f2a38608d792bf0f
digmi123 <65775879+digmi123@users.noreply.github.com>
2025-02-18 14:15:14 +0200

    Update Divider Styling.

--- client/src/shared/components/Divider.jsx
+++ client/src/shared/components/Divider.jsx
@@ -2,7 +2,7 @@ function Divider() {
   return (
     <hr
       id="divider"
-      className="w-full h-[1px] bg-slate-300 rounded-md my-4 border-0"
+      className="w-full h-[1px] bg-slate-300 rounded-md my-4 border-1"
     />
   );
 }
