5a606e201e7e79afc8eb16dc0f0b269449191e5a
digmi123 <65775879+digmi123@users.noreply.github.com>
2025-02-18 14:21:44 +0200

    Create OrganizationParticipant to act as a wrapper to members component.

---
+++ client/src/features/organizations/components/OrganizationParticipant.jsx
@@ -0,0 +1,9 @@
+import Members from "@/features/screens/Members";
+
+function OrganizationParticipant() {
+  //   const { members, loading } = useOrganizationMembers();
+  //   return <Members members={members} loading={loading} />;
+  return <h1>OrganizationParticipant</h1>;
+}
+
+export default OrganizationParticipant;
