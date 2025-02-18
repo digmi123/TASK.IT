72ed2055b750b1e8d4049b285807aeb91ae550b8
digmi123 <65775879+digmi123@users.noreply.github.com>
2025-02-18 13:59:17 +0200

    Update NewMemberAvatar component ui.

--- client/src/features/members/components/NewMemberAvatar.jsx
+++ client/src/features/members/components/NewMemberAvatar.jsx
@@ -1,19 +1,28 @@
 import { cn } from "@/lib/utils";
+import { forwardRef } from "react";
 
-function NewMemberAvatar({ email, className }) {
-  const firstLetter = email.toUpperCase()[0];
+const NewMemberAvatar = forwardRef(({ user, className }, ref) => {
+  const firstLetter = user.email.toUpperCase()[0];
 
   return (
-    <div
-      id="avatar-wrapper"
-      className={cn(
-        "w-[48px] h-[48px] flex items-center justify-center text-center bg-sky-500 rounded-full",
-        className
+    <div className={cn("w-8 h-8 min-w-8 min-h-8", className)} ref={ref}>
+      {user.picture ? (
+        <img
+          src={user.picture}
+          alt="user-avatar"
+          className="w-full h-full rounded-full"
+        />
+      ) : (
+        <div
+          id="avatar-wrapper"
+          className="w-full h-full flex items-center justify-center text-center bg-sky-500 rounded-full aspect-square"
+        >
+          <p className="text-xl text-white">{firstLetter}</p>
+        </div>
       )}
-    >
-      <p className="text-xl text-white">{firstLetter}</p>
     </div>
   );
-}
+});
 
+NewMemberAvatar.displayName = "NewMemberAvatar";
 export default NewMemberAvatar;
