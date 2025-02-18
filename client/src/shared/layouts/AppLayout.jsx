1795ee404e7f3b40e4776202e4721bb64caeeaa5
digmi123 <65775879+digmi123@users.noreply.github.com>
2025-02-18 14:03:23 +0200

    Update AppLayout UI.

--- client/src/shared/layouts/AppLayout.jsx
+++ client/src/shared/layouts/AppLayout.jsx
@@ -2,16 +2,16 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { Provider } from "react-redux";
 import { store } from "@/redux/store";
-import SideBar from "@/features/desks/components/Sidebar";
+import SideBar from "@/shared/components/SideBar";
 
 function AppLayout() {
   return (
     <Provider store={store}>
       <div id="app-wrapper" className="h-screen overflow-auto flex flex-col">
         <Navbar />
-        <div className="flex gap-16 py-8 px-12 h-full overflow-auto">
+        <div className="flex gap-16 h-full overflow-auto">
           <SideBar />
-          <main className="flex flex-col gap-6 overflow-auto flex-grow pr-4">
+          <main className="flex flex-col gap-6 overflow-auto flex-grow pr-4 py-8 px-12">
             <Outlet />
           </main>
         </div>
