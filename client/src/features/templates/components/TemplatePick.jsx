1c10d576eec9da46ada150b0d2b67f2a56d84e0d
digmi123 <65775879+digmi123@users.noreply.github.com>
2025-02-24 16:54:56 +0200

    Update TemplatePick component.

--- client/src/features/templates/components/TemplatePick.jsx
+++ client/src/features/templates/components/TemplatePick.jsx
@@ -1,30 +1,33 @@
 import { cn } from "@/lib/utils";
+import { forwardRef } from "react";
 
-function TemplatePick({ templateName, backgroundImage, className }) {
-  return (
-    <div
-      id="template-pick"
-      className={cn(
-        "overflow-hidden w-56 h-24 rounded-md flex-shrink-0",
-        className
-      )}
-      style={{
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
+const TemplatePick = forwardRef(
+  ({ templateName, backgroundImage, className, ...props }, ref) => {
+    return (
       <div
-        id="inner-template-card"
-        className="py-2 px-4 h-full peer-not-checked:backdrop-blur-sm"
+        ref={ref}
+        id="template-pick"
+        className={cn(
+          "overflow-hidden w-56 h-24 rounded-md flex-shrink-0 cursor-pointer",
+          className
+        )}
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+        {...props}
       >
-        <h2 className="text-white text-sm bg-primary px-2 py-1 rounded-md w-fit">
-          Template
-        </h2>
-        <h3 className="text-inherit font-bold">{templateName}</h3>
+        <div id="inner-template-card" className="py-2 px-4 h-full">
+          <h2 className="text-white text-sm bg-primary px-2 py-1 rounded-md w-fit">
+            Template
+          </h2>
+          <h3 className="text-inherit font-bold">{templateName}</h3>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+);
 
+TemplatePick.displayName = "TemplatePick";
 export default TemplatePick;
