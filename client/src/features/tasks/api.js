aace2bb49d1918e3554c32c5c27123e19d58c971
digmi123 <65775879+digmi123@users.noreply.github.com>
2025-02-26 18:12:56 +0200

    Delete task endpoint on backend.

--- client/src/features/tasks/api.js
+++ client/src/features/tasks/api.js
@@ -13,3 +13,13 @@ export const addTask = async ({ task }) => {
   const { data: addedTask } = await axios.post("/api/tasks", { task });
   return addedTask;
 };
+
+export const deleteTask = async (task) => {
+  const { id: taskId } = task;
+  try {
+    await axios.delete(`/api/tasks/${taskId}`);
+  } catch (error) {
+    console.error("Error deleting task:", error);
+    throw new Error(error.response?.data?.message || "Failed to delete task");
+  }
+};
